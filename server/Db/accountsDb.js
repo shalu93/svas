42534ccc435ed2a3c1b19b4dc2b070ddd7dd4a3c
shalu chandwani <shaluchandwani@rocketmail.com>
2019-04-18 04:46:30 +0200

    bg eslint fixes
    - fixed all the errors
    
    [finishes #165436744]

--- server/Db/accountsDb.js
+++ server/Db/accountsDb.js
@@ -1,42 +1,42 @@
 const accountDb = [
     {
         accountNumber: 1,
-        firstName: "shalu",
-        lastName : "chandwani",
-        email: "shaluchandwani@svasbanka.com",
-        type: "savings",
-        status:"active",
+        firstName: 'shalu',
+        lastName : 'chandwani',
+        email: 'shaluchandwani@svasbanka.com',
+        type: 'savings',
+        status:'active',
         openingBalance:100
     },
     {
         accountNumber: 2,
-        firstName: "pankaj",
-        lastName : "vaswani",
-        email: "pankajvaswani55@svasbanka.com",
-        type: "current",
-        status:"draft",
+        firstName: 'pankaj',
+        lastName : 'vaswani',
+        email: 'pankajvaswani55@svasbanka.com',
+        type: 'current',
+        status:'draft',
         openingBalance:1000
     },
 
     {
         accountNumber: 3,
-        firstName: "abhinav",
-        lastName : "sharma",
-        email: "abhisharma@svasbanka.com",
-        type: "current",
-        status:"dormant",
+        firstName: 'abhinav',
+        lastName : 'sharma',
+        email: 'abhisharma@svasbanka.com',
+        type: 'current',
+        status:'dormant',
         openingBalance:5000
     },
 
     {
         accountNumber: 4,
-        firstName: "madhuri",
-        lastName : "S vaswani",
-        email: "madhuris@svasbanka.com",
-        type: "savings",
-        status:"active",
+        firstName: 'madhuri',
+        lastName : 'S vaswani',
+        email: 'madhuris@svasbanka.com',
+        type: 'savings',
+        status:'active',
         openingBalance:600
     }
-]
+];
 
 export {accountDb};
