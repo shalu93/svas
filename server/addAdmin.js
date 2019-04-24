25477a9df5db0380bf0c96d78672ebc9ad3592c1
shalu chandwani <shaluchandwani@rocketmail.com>
2019-04-24 11:37:34 +0200

    adding addadmin

--- server/addAdmin.js
+++ server/addAdmin.js
@@ -1,9 +1,16 @@
-const db = require('db');
+const db = require('./db');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const addAdminuser = (req, res, next) => {
-    const text = `INSERT INTO users(firstname, lastname, email, password, usertype) VALUES($1, $2, $3, $4, $5) RETURNING *`;
-    const values = ['shalu', 'chandwani', 'shaluchandwani@rocketmail.com', 'shalu@1993', 'admin'];
+    const text = `INSERT INTO users(userid,firstname, lastname, email, password, usertype) VALUES($1, $2, $3, $4, $5,$6) RETURNING *`;
+    const values = ['12','shalu', 'chandwani', 'shaluchandwani@rocketmail.com', 'shalu@1993', 'admin'];
     
     db.query(text, values);
-    next();
 }
+
+module.exports = {
+    addAdminuser
+};
+
+require('make-runnable');
--- server/routes/tranRoutes.js
+++ server/routes/tranRoutes.js
@@ -8,6 +8,6 @@ tranRouter.post('/transactions/:accountNumber/debit',auth.authorization,transact
 tranRouter.post('/transactions/:accountNumber/credit',auth.authorization,transactions.creditAccount);
 
 tranRouter.get('/accounts/:accountNumber/transactions',auth.authorization,transactions.viewTransaction);
-tranRouter.get('/transactions/:transactionid/',auth.authorization,transactions.viewTransactionid);
+tranRouter.get('/transactions/:transactionid',auth.authorization,transactions.viewTransactionid);
 
 export default tranRouter;
